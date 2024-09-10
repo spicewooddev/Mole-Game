21dece789b122176ec41f3a32ff1b504a608d90a
spicewooddev <shloth03@gmail.com>
2024-09-09 22:22:02 -0500

    Rewrote Code For Efficiency & Created Raindrops Particle System
    sorry i wasn't able to do much earlier today! i was doing some school stuff and drew some of the assets. but i basically rewrote the code for the mole game script to be more efficient so we don't have to do the Sprite.Create() stuff or the Resources.Load() stuff, which is a lot of work for a computer to do. I made a Particle System Prefab for the Raindrops which you can check out and use for yourself. It doesn't look the exact same but I tried to make it as close as possible to the original, but you can tweak it if you would like. In addition, I made a TileType script which is just a barebones constructor script for the tiles, where we can add more features to it later. Let me know what you think!

--- Mole Game - Brackeys Game Jam 2024/Assets/Scripts/Mole Game.cs
+++ Mole Game - Brackeys Game Jam 2024/Assets/Scripts/Mole Game.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
 enum TileType
 {
     Dirt,
@@ -9,114 +10,66 @@ enum TileType
     Open,
     Ladder
 }
+*/
 
 public class MoleGame : MonoBehaviour
 {
-    TileType[,] tileTypes = new TileType[16, 16];
-    GameObject[,] tileSprites = new GameObject[16, 16];
+    public TileType[] tileTypes;
+    public GameObject[,] tileSprites = new GameObject[24, 24];
+    public int[,] tiles = new int[24, 24];
 
-    // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < tileTypes.GetLength(0); i++)
+        GenerateMapData();
+        GenerateMapVisuals();
+    }
+
+    void GenerateMapData()
+    {
+        //initialize all tiles in environment
+        for (int row = 0; row < tiles.GetLength(0); row++)
         {
-            for (int j = 0; j < tileTypes.GetLength(1); j++)
+            for (int column = 0; column < tiles.GetLength(1); column++)
             {
                 int pickTileType = UnityEngine.Random.Range(0, 3);
-                if (pickTileType == 0) {
-                    tileTypes[i,j] = TileType.Dirt;
-                } else if (pickTileType == 1) {
-                    tileTypes[i,j] = TileType.Rock;
-                } else {
-                    tileTypes[i,j] = TileType.Open;
-                }
-            }
-        }
 
-        for (int i = 0; i < tileSprites.GetLength(0); i++)
-        {
-            for (int j = 0; j < tileSprites.GetLength(1); j++)
-            {
-                GameObject go = new GameObject("Tile Sprite");
-                tileSprites[i,j] = go;
-                
-                go.transform.position = new Vector3(i-8, j-8, 0);
-                go.transform.localScale = new Vector2(0.5f, 0.5f);
-                go.transform.position = new Vector3((i-32) / 2.0f, (j-32) / 2.0f, 0);
-                
-                SpriteRenderer renderer = go.AddComponent<SpriteRenderer>();
-                Texture2D texture = Resources.Load<Texture2D>("blank_square");
-                Sprite sprite = Sprite.Create
-                (
-                    texture,
-                    new UnityEngine.Rect(0.0f,0.0f,texture.width,texture.height),
-                    new Vector2(0.5f, 0.5f),
-                    (float) texture.width
-                );
-                renderer.sprite = sprite;
-
-                if (tileTypes[i,j] == TileType.Rock)
+                if (pickTileType == 1)
                 {
-                    Rigidbody2D body = go.AddComponent<Rigidbody2D>();
-                    body.gravityScale = 0f;
-                    body.bodyType = RigidbodyType2D.Static;
-                    BoxCollider2D collider = go.AddComponent<BoxCollider2D>();                    
+                    //dirt wall tiles
+                    tiles[row, column] = 1;
                 }
-            }
-        }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        UpdateTileSprites();
-
-        // Make a single rain drop (a whole bunch of times).
-        GameObject go = new GameObject("Rain Drop");
-        go.transform.localScale = new Vector2(0.1f, 0.1f);
-        go.transform.position = new Vector2(UnityEngine.Random.Range(-6, 6), 64);
-        
-        SpriteRenderer renderer = go.AddComponent<SpriteRenderer>();
-        renderer.color = Color.blue;                    
-        Texture2D texture = Resources.Load<Texture2D>("blank_square");
-        Sprite sprite = Sprite.Create
-        (
-            texture,
-            new UnityEngine.Rect(0.0f,0.0f,texture.width,texture.height),
-            new Vector2(0.5f, 0.5f),
-            (float) texture.width
-        );
-        renderer.sprite = sprite;
-
-        go.AddComponent<Rigidbody2D>();
-        go.AddComponent<BoxCollider2D>();
-    }
-
-    void UpdateTileSprites()
-    {           
-        for (int i = 0; i < tileTypes.GetLength(0); i++)
-        {
-            for (int j = 0; j < tileTypes.GetLength(1); j++)
-            {
-                GameObject go = tileSprites[i,j];
-                SpriteRenderer renderer = go.GetComponent<SpriteRenderer>();
-                if (tileTypes[i,j] == TileType.Dirt)
+                else if (pickTileType == 2)
                 {
-                    renderer.color = new Color(139f / 256f, 69f / 256f, 19f / 256f, 1.0f);
+                    //stone wall tiles
+                    tiles[row, column] = 2;
                 }
-                else if (tileTypes[i,j] == TileType.Rock)
-                {
-                    renderer.color = Color.gray;
-                }
-                else if (tileTypes[i,j] == TileType.Open)
-                {
-                    renderer.color = Color.black;
-                }
+
                 else
                 {
-                    renderer.color = Color.red;                    
+                    //empty tiles
+                    tiles[row, column] = 0;
                 }
             }
         }
+    }
+
+
+    void GenerateMapVisuals()
+    {
+        for (int row = 0; row < tiles.GetLength(0); row++)
+        {
+            for (int column = 0; column < tiles.GetLength(1); column++)
+            {
+                TileType tileType = tileTypes[tiles[row, column]];
+                GameObject go = (GameObject)Instantiate(tileType.go, new Vector3((row - 12) * 0.45f, (column - 12) * 0.45f, 0), Quaternion.identity);
+                go.transform.localScale = new Vector2(2f, 2f);
+            }
+        }
+    }
+
+    void Update()
+    {
+
     }
 }
